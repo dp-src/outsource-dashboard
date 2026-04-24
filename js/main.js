eb0adf797022d06efc00f3e817c27cc9b37e1afb
Darya Panova <panovadv24@gmail.com>
2026-04-24 19:20:15 +0500

    Feat: implement tab switching functionality in sidebar navigation

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// switch tabs
+
+const navTabs = document.querySelector(".sidebar-nav");
+const tabs = document.querySelectorAll(".nav-item");
+const contents = document.querySelectorAll(".contents");
+
+function switchTab(targetId) {
+  contents.forEach((content) => {
+    content.classList.add("hidden");
+  });
+
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+
+  const activeTab = document.querySelector(`[data-tab="${targetId}"]`);
+  const activeContent = document.querySelector(`#${targetId}-content`);
+
+  if (activeContent) {
+    activeContent.classList.remove("hidden");
+  }
+
+  if (activeTab) {
+    activeTab.classList.add("active");
+  }
+}
+
+navTabs.addEventListener("click", (event) => {
+  const clickedTab = event.target.closest(".nav-item");
+
+  if (clickedTab) {
+    const tabId = clickedTab.dataset.tab; // Берем id из data-attribute
+    switchTab(tabId);
+  }
+});
