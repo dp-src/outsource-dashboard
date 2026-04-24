5d9332019f6c6d7444ca91709910d9452acd5661
Darya Panova <panovadv24@gmail.com>
2026-04-24 22:24:08 +0500

    Feat: implement sidebar toggle functionality with open/close buttons

--- js/main.js
+++ js/main.js
@@ -33,3 +33,24 @@ navTabs.addEventListener("click", (event) => {
     switchTab(tabId);
   }
 });
+
+// sidebar
+
+const sidebar = document.querySelector(".sidebar");
+const sidebarCloseButton = document.querySelector(".sidebar-close-button");
+const sidebarOpenButton = document.querySelector(".sidebar-open-button");
+
+document.addEventListener("click", (event) => {
+  const closeTarget = event.target.closest(".sidebar-close-button");
+  const openTarget = event.target.closest(".sidebar-open-button");
+
+  if (closeTarget) {
+    sidebar.classList.add("collapsed");
+    sidebarOpenButton.classList.remove("hidden");
+  }
+
+  if (openTarget) {
+    sidebar.classList.remove("collapsed");
+    sidebarOpenButton.classList.add("hidden");
+  }
+});
